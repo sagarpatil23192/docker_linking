507f0351326912b0cdb0d6419a008a1798d587dd
root <root@ip-172-31-11-156.ec2.internal>
2017-06-20 07:21:58 +0000

    Adding code

--- mysql-service/start.sh
+++ mysql-service/start.sh
@@ -3,7 +3,7 @@
 # Run the MySQL container, with a database named 'users' and credentials
 # for a users-service user which can access it.
 echo "Starting DB..."  
-docker run --name db -d -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=<DB_name> -e MYSQL_USER=root -e MYSQL_PASSWORD=root  -p 3306:3306  mysql:latest
+docker run --name db -d -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=domo_mom_db -e MYSQL_USER=user1 -e MYSQL_PASSWORD=root  -p 3306:3306  mysql:latest
 
 # Wait for the database service to start up.
 echo "Waiting for DB to start up..."  
